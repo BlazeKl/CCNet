62ad80bcac262cf6cd5d2b6d4503cc7f6801507d
Axeljerez24 <63621637+Axeljerez24@users.noreply.github.com>
2020-06-25 21:41:51 -0500

    Update urls.py

--- ComunidadCautivaNet/urls.py
+++ ComunidadCautivaNet/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import login,logout
 
 urlpatterns = [
     path('Net/', include('Net.urls')),
     path('admin/', admin.site.urls),
+    path('login/', login,{'template_name':'login.html'}, name = 'login'),
 ]

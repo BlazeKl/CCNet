9d6e49289795e460f7cdb5451ea4dcbe91cdbc44
BlazeKl <felipe.perezc@utem.cl>
2020-08-01 21:29:21 -0400

    nueva secret key no compartida

--- ComunidadCautivaNet/settings.py
+++ ComunidadCautivaNet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from ComunidadCautivaNet.secrets.local_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,11 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a_s&-t+5irzz6x3qpm#w8q@8=z484dy$2fv13zf7wiryb3%t2n'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
